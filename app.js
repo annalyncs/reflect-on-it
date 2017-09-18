91958441c86c2b09308f30b36c7696fe7d038421
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-09-17 19:12:37 -0700

    added mock data

---
+++ app.js
@@ -0,0 +1,32 @@
+var MOCK_SUBMISSIONS = {
+    "submissions": [
+        {
+            "id": "111111",
+            "text": "hello world",
+            "mood": "happy",
+            "date": "09/17/2017",
+            "location": "home",
+        },
+        {
+            "id": "1234456",
+            "text": "new entry submission hello",
+            "mood": "sad",
+            "date": "01/26/2017",
+            "location": "work",
+        },
+        {
+            "id": "333333",
+            "text": "reflection reflecting new reflection is writtten here",
+            "mood": "angry",
+            "date": "04/08/2017",
+            "location": "school",
+        },
+        {
+            "id": "456789",
+            "text": "sample text sample reflection sample sample",
+            "mood": "calm",
+            "date": "07/07/2017",
+            "location": "work",
+        },
+    ]
+}
