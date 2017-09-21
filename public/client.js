be22bb853b29bde644a5b2255fe37be934c0c0f2
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-09-20 21:28:09 -0700

    client function to post form submissions

--- public/client.js
+++ public/client.js
@@ -1,14 +1,26 @@
-var REFLECTIONS_URL = '/reflections';
-var NEW_REFLECTIONS_URL = '/reflections/new';
+let REFLECTIONS_URL = '/reflections';
+let NEW_REFLECTIONS_URL = '/reflections/new';
+
+
 
 function postNewReflection(event) {
     $('#new-reflection').on('submit', function (e) {
         e.preventDefault();
         $.ajax(NEW_REFLECTIONS_URL, {
             type: "POST",
-            data: $(this).serialize(),
-            success: function () {
-                console.log('posting data');
+            success: function (data) {
+                let str = $('#new-reflection').serializeArray();
+                console.log(str);
+                $('#reflections').append(`
+                <h2>Date:</h2>
+                <p class="reflection-date">${str[0].value}</p>
+                <h2>Location:</h2>
+                <p class="reflection-location">${str[1].value}</p>
+                <h2>Mood:</h2>
+                <p class="reflection-mood">${str[2].value}</p>
+                <h2>Reflection:</h2>
+                <p class="reflection-text">${str[3].value}</p>
+                `);
             }
         });
     });
