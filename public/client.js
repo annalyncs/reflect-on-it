f7c9480dd526fc6a610f54f601aa343d04298c75
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-10-25 21:00:41 -0700

    updated  dropdown menu CSS and HTML

--- public/client.js
+++ public/client.js
@@ -307,7 +307,7 @@ function handleUpdateReflection() {
 //navigation buttons
 
 function handleNavCreateButton() {
-    $('#nav-create-button').click(function () {
+    $('.dropdown-content').on('click', '#nav-create-button', function () {
         $('form#new-reflection :input').val("");
         $('#new-entry').removeClass('hide-display').html(` <form method="post" id="new-reflection">
             <fieldset>
@@ -354,6 +354,7 @@ function handleNavResourcesButton() {
 }
 
 
+
 $(function () {
     postNewReflection();
     handleDeleteReflections();
@@ -366,3 +367,24 @@ $(function () {
     handleNavResourcesButton();
     handleNewEntryButton();
 })
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
